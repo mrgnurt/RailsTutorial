6fbfd3d38778d9daf3f948bb539084288aefd18d
mrgnurt <mrgnurt1995@gmail.com>
2017-07-07 12:06:39 +0700

    chap8

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 
   get "/signup",  to: "users#new"
 
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   resources :users, only: [:new, :create, :show]
 end
