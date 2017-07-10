66f61209243250d5df9c5ba68fa8abc168fd8852
mrgnurt <mrgnurt1995@gmail.com>
2017-07-10 08:50:58 +0700

    chap9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_accessor :remember_token
 
   validates :name,
     presence: true,
@@ -10,7 +11,8 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password,
     presence: true,
-    length: {minimum: Settings.password_max_length}
+    length: {minimum: Settings.password_max_length},
+    allow_nil: true
 
   has_secure_password
 
@@ -18,5 +20,35 @@ class User < ApplicationRecord
 
   def downcase_email
     email.downcase
+  end
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest.present?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 end
