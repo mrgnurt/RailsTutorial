d6480a7c12e509e615f908bd3b96a4dd4ff98832
mrgnurt <mrgnurt1995@gmail.com>
2017-07-14 13:17:57 +0700

    chap14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+    foreign_key: "follower_id",
+    dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+    foreign_key: "followed_id",
+    dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   attr_accessor :remember_token, :activation_token, :reset_token
@@ -91,5 +100,17 @@ class User < ApplicationRecord
 
   def feed
     microposts.newest_first
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
